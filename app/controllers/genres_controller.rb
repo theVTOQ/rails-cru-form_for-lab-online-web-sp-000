cac38d9d90430a7f139104c03b3239d565b88fe6
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 05:26:50 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,16 @@
 class GenresController < ApplicationController
   def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name, :bio))
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name, :bio))
+    redirect_to genre_path(@genre)
   end
 end
