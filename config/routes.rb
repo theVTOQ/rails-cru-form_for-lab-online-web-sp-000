4938388c36ce26ab8712ed8bc324ae636e09c1eb
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 06:11:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   patch '/artists/:id', to: "artists#update"
   resources :genres, only: [:new, :create, :edit,  :show]
   patch '/genres/:id', to: "genres#update"
-  resources :songs, only: [:new, :create, :edit, :show]
+  resources :songs, only: [:index, :new, :create, :edit, :show]
   patch '/songs/:id', to: "songs#update"
 end
