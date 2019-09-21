4764424914ad7e788de6414d5d489fafbb2b05ac
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 05:21:22 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.create()
+    @artist = Artist.create(params.require(:artist).permit(:name))
     redirect_to artist_path(@artist)
   end
 
