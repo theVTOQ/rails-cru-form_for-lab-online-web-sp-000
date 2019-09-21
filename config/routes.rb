6ad8e00d735f59b1b6bdd2512ddeffda3fb1ab9c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 05:19:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :artists, only: [:create, :update, :show]
   resources :genres, only: [:create, :update, :show]
