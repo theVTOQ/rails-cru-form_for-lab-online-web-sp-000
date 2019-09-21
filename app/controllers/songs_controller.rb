c22e8b3cbf9f6b0f792e8fccdd3f9e1fea3bad9a
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 06:07:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,7 +17,7 @@ class SongsController < ApplicationController
   end
 
   def update
-    @song = song.find(params[:id])
+    @song = Song.find(params[:id])
     @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
