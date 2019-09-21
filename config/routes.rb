bda83bda7406014a980cb1c8bf23b1e1ec3ea6c7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 06:00:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'songs/show'
-
   get 'genres/show'
 
   get 'artists/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists, only: [:create, :update, :show]
-  resources :genres, only: [:create, :update, :show]
-  resources :songs, only: [:create, :update, :show]
+  resources :artists, only: [:new, :create, :edit, :show]
+  patch '/artists/:id', to: "artists#update"
+  resources :genres, only: [:new, :create, :edit,  :show]
+  patch '/genres/:id', to: "genres#update"
+  resources :songs, only: [:new, :create, :edit, :show]
+  patch '/songs/:id', to: "songs#update"
 end
