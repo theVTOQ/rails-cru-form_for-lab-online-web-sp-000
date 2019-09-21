05a72f6f8e0a39cae68d623e3e3580045943e969
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 05:11:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists, only: [:create, :new, :show]
-  resources :genres, only: [:create, :new, :show]
-  resources :songs, only: [:create, :new, :show]
+  resources :artists, only: [:create, :update, :show]
+  resources :genres, only: [:create, :update, :show]
+  resources :songs, only: [:create, :update, :show]
 end
